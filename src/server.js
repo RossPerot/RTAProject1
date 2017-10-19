0973555bbb3f3dc43c0cdb9fa7b00a28677157c7
RossPerot <rmh8980@g.rit.edu>
2017-10-18 21:03:28 -0400

    finished tic-tac-toe

--- src/server.js
+++ src/server.js
@@ -19,18 +19,9 @@ const io = socketio(server);
 
 let turn = 'X';
 let numOfPlayers = 0;
+let rooms = 0;
 
-const tictactoe = {
-  1: '',
-  2: '',
-  3: '',
-  4: '',
-  5: '',
-  6: '',
-  7: '',
-  8: '',
-  9: '',
-};
+const tictactoe = {};
 
 server.listen(PORT, (err) => {
   if (err) {
@@ -39,23 +30,32 @@ server.listen(PORT, (err) => {
   console.log(`Listening on port ${PORT}`);
 });
 
-const checkForWinner = (player) => {
+const checkForWinner = (player, room) => {
+  const currentBoard = tictactoe[`room${room}`].split(',');
   let winner = false;
-  if (tictactoe[1] === player && tictactoe[2] === player && tictactoe[3] === player) {
+  if (currentBoard[0] === player && currentBoard[1] ===
+      player && currentBoard[2] === player) {
     winner = true;
-  } else if (tictactoe[4] === player && tictactoe[5] === player && tictactoe[6] === player) {
+  } else if (currentBoard[3] === player && currentBoard[4] ===
+             player && currentBoard[5] === player) {
     winner = true;
-  } else if (tictactoe[7] === player && tictactoe[8] === player && tictactoe[9] === player) {
+  } else if (currentBoard[6] === player && currentBoard[7] ===
+             player && currentBoard[8] === player) {
     winner = true;
-  } else if (tictactoe[1] === player && tictactoe[4] === player && tictactoe[7] === player) {
+  } else if (currentBoard[0] === player && currentBoard[3] ===
+             player && currentBoard[6] === player) {
     winner = true;
-  } else if (tictactoe[2] === player && tictactoe[5] === player && tictactoe[8] === player) {
+  } else if (currentBoard[1] === player && currentBoard[4] ===
+             player && currentBoard[7] === player) {
     winner = true;
-  } else if (tictactoe[3] === player && tictactoe[6] === player && tictactoe[7] === player) {
+  } else if (currentBoard[2] === player && currentBoard[5] ===
+             player && currentBoard[8] === player) {
     winner = true;
-  } else if (tictactoe[1] === player && tictactoe[5] === player && tictactoe[9] === player) {
+  } else if (currentBoard[0] === player && currentBoard[4] ===
+             player && currentBoard[8] === player) {
     winner = true;
-  } else if (tictactoe[3] === player && tictactoe[5] === player && tictactoe[7] === player) {
+  } else if (currentBoard[2] === player && currentBoard[4] ===
+             player && currentBoard[6] === player) {
     winner = true;
   }
   return winner;
@@ -64,32 +64,39 @@ const checkForWinner = (player) => {
 io.on('connection', (sock) => {
   const socket = sock;
   if (numOfPlayers === 0) {
-    socket.join('room1');
-    const data = { player: 'X' };
+    socket.join(`room${rooms}`);
+    tictactoe[`room${rooms}`] = ' , , , , , , , , ';
+    const data = { player: 'X', room: rooms, tictactoe: tictactoe[`room${rooms}`]};
+    console.log(`${rooms} ${numOfPlayers}`);
     socket.emit('joined', data);
     numOfPlayers++;
   } else if (numOfPlayers === 1) {
-    socket.join('room1');
-    const data = { player: 'O' };
+    socket.join(`room${rooms}`);
+    const data = { player: 'O', room: rooms, tictactoe: tictactoe[`room${rooms}`] };
+    console.log(`${rooms} ${numOfPlayers}`);
     socket.emit('joined', data);
-    numOfPlayers++;
+    numOfPlayers = 0;
+    rooms++;
   }
 
   socket.on('play', (data) => {
-        if (data.player === turn) {
-            tictactoe[data.location] = data.player;
-            if (checkForWinner(turn)) {
-                io.sockets.in('room1').emit('winner', {player: data.player });
-                io.sockets.in('room1').emit('playFromServer', tictactoe);
-                return;
-            }
-            console.log(tictactoe);
-            io.sockets.in('room1').emit('playFromServer', tictactoe);
-            if (turn === 'X') turn = 'O';
-            else if (turn === 'O') turn = 'X';
-        }
+    if (data.player === turn) {
+      let currentBoard = tictactoe[`room${data.room}`];
+      const tempBoard = currentBoard.split(',');
+      tempBoard[data.location] = data.player;
+      currentBoard = tempBoard.join();
+      tictactoe[`room${data.room}`] = currentBoard;
+      if (checkForWinner(turn, data.room)) {
+        io.sockets.in(`room${data.room}`).emit('winner', { player: data.player });
+        io.sockets.in(`room${data.room}`).emit('playFromServer', tictactoe[`room${data.room}`]);
+        return;
+      }
+      io.sockets.in(`room${data.room}`).emit('playFromServer', tictactoe[`room${data.room}`]);
+      if (turn === 'X') turn = 'O';
+      else if (turn === 'O') turn = 'X';
+    }
   });
-    socket.on('disconnect', () =>{
-        numOfPlayers--;
-    })
+  socket.on('disconnect', () => {
+    numOfPlayers--;
+  });
 });
